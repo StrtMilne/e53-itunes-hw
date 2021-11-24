c72b32fb324d785b8c66b15bd6b0e5a5814997ec
StrtMilne <oober@hotmail.co.uk>
2021-11-24 18:13:05 +0000

    added art

--- src/components/Song.js
+++ src/components/Song.js
@@ -3,8 +3,10 @@ import React from "react";
 const Song = ({song}) => {
     return(
         <li>
+            <img src={song["im:image"][2].label} alt="album art"/>
             <h2>{song["im:name"].label}</h2>
             <h3>{song["im:artist"].label}</h3>
+            <audio src={song["link"][1]["attributes"]["href"]}/>
         </li>
     )
 }
