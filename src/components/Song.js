ad2b1a3f1f9561ddee7f97afa21107acc756c9c9
StrtMilne <oober@hotmail.co.uk>
2021-11-24 18:26:45 +0000

    added player for preview

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 const Song = ({song}) => {
+
     return(
         <li>
             <img src={song["im:image"][2].label} alt="album art"/>
-            <h2>{song["im:name"].label}</h2>
-            <h3>{song["im:artist"].label}</h3>
-            <audio src={song["link"][1]["attributes"]["href"]}/>
+            <section>
+                <h2>{song["im:name"].label}</h2>
+                <h3>{song["im:artist"].label}</h3>
+                <audio controlsList="nodownload" controls>
+                    <source src={song["link"][1]["attributes"]["href"]}/>
+                </audio>
+            </section>
         </li>
     )
 }
